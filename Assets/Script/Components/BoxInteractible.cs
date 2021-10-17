c23dc2340d36e0998e399b89ff19b3b6b026457a
Björn Frohberg <bfrohberg@web.de>
2021-10-17 03:10:14 +0200

    fix depth pf field - not working - disabled

---
+++ Assets/Script/Components/BoxInteractible.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21b44d0e0b49a33faf0bf4093a0be65a95105b4d8d238856183964625a780b9e
+size 1515
--- Assets/Script/Components/Model/EObjectType.cs
+++ Assets/Script/Components/Model/EObjectType.cs
@@ -1,15 +1,3 @@
-﻿namespace Assets.Script.Components
-{
-		public enum EObjectType
-		{
-				UNDEFINED,
-				BALLOON,
-				DOOR,
-				CANDLE,
-				FLASHLIGHT,
-				LIGHTER,
-				DOTS_PRJECTOR,
-				POWER_UP,
-				CAMERA
-		}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8168e8f0cba57ff41e9f8fffd5e445bf20f70b6afc14d34cde7e4025fe045f3a
+size 224
